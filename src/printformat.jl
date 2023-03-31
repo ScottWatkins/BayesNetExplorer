df6f41a9e6a91f9ab88a17bf74145655a9247ff5
Scott Watkins <scott.watkins@genetics.utah.edu>
2023-03-31 12:27:09 -0600

    Delete src directory

--- src/printformat.jl
+++
@@ -1,9 +0,0 @@
-"""
-    printformat(n; fmt=f)
-
-Format a variable with printf. The printf format
-string is passed as a variable.
-"""
-function printformat(n; fmt="")
-    Printf.format(Printf.Format(fmt), n)
-end
